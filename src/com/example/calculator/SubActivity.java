68e028346a253342752dff44536c55710db4e6af
shogohciai <shogo1104@gmail.com>
2013-07-24 15:57:09 +0900

    戻れる

--- src/com/example/calculator/SubActivity.java
+++ src/com/example/calculator/SubActivity.java
@@ -6,37 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.example.calculator.MainActivity;
 
-	public class SubActivity extends Activity implements OnClickListener{  
+public class SubActivity extends Activity{  
 
-		@Override  
-	    protected void onCreate(Bundle savedInstanceState) {  
-	        super.onCreate(savedInstanceState);  
-	        setContentView(R.layout.activity_sub);  
-	    
-	        final Button bt2 = (Button) findViewById(R.id.ButtonSub);
-	        bt2.setOnClickListener(new OnClickListener(){
-	        	public void onClick(View v){
-	    			Intent i = new Intent();
-    				i.setClassName("com.example.calculator","com.example.calculator.MainActivity");
-    				i.putExtra("com.example.calculator.testString", "!TEST STRING!");
-    				startActivity(i);
-    				}
-	        });
-	        
-	        
-	        /**インテントより渡された値を取得します。  
-	        Intent i = getIntent();  
-	        if (i != null){
-	            String str = i.getStringExtra("com.example.calculator.intent.testString");
-	            System.out.println(str);
-	        }  */
-	    }
+	@Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_sub);
+    
+        final Button bt2 = (Button) findViewById(R.id.ButtonSub);
 
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			
-		}  
-
-	}
+        bt2.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		intentMethod();
+        		}
+        	private void intentMethod(){
+	        	Intent i = new Intent();
+				i.setClassName("com.example.calculator","com.example.calculator.MainActivity");
+				i.putExtra("com.example.calculator.testString", "戻りました");
+				startActivity(i);
+        	}
+        });
+    }
+}
